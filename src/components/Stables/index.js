a9e9e31c9dab083feb919b7976c0af3f9e48e986
Benn King <bennkingy@gmail.com>
2022-04-18 10:37:12 +0100

    updates

--- src/components/Stables/index.js
+++ src/components/Stables/index.js
@@ -10,38 +10,6 @@ import {
 
 const Stables = () => {
 
-
-// usetstate for storing and retrieving wallet details
-const [data, setdata] = useState({
-  address: "",
-  Balance: null,
-});
-
-// Button handler button for handling a
-// request event for metamask
-const btnhandler = () => {
-
-  // Asking if metamask is already present or not
-  if (window.ethereum) {
-    // res[0] for fetching a first wallet
-    window.ethereum
-      .request({ method: "eth_requestAccounts" })
-      .then((res) => getbalance(res[0]));
-  } else {
-    alert("install metamask extension!!");
-  }
-};
-
-  // getbalance function for getting a balance in
-  // a right format with help of ethers
-  const getbalance = (address) => {
-    axios(
-     // `https://api.opensea.io/api/v1/assets?owner=${address}`,
-     `https://api.opensea.io/api/v1/assets?owner=0x0d2e83712ec165b90F3316D5441D7deE55B5794a`,
-      { method: "GET", headers: { Accept: "application/json" } }
-    ).then(response => console.log(response))
-  };
-
   return (
     <>
       <ServicesContainer id='stables'>
